0e5de0dbefdc2a3c5e0e6656aa6e8bbf230d1911
hiltonchang <hiltonchiang@gmail.com>
2025-01-09 13:34:59 +0800

    add combo.mdx

--- app/Main.tsx
+++ app/Main.tsx
@@ -21,7 +21,7 @@ export default function Home({ posts }) {
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
-            const { slug, date, title, summary, tags } = post
+            const { slug, date, lastmod, title, summary, tags } = post
             return (
               <li key={slug} className="py-12">
                 <article>

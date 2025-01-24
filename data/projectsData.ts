6a2f66b771b5e0094d19240e64df123b747058bb
hiltonchang <hiltonchiang@gmail.com>
2025-01-24 08:37:36 +0800

    added black-phone

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -8,7 +8,7 @@ interface Project {
 const projectsData: Project[] = [
   {
     title: 'Peer-to-Peer VoIP',
-    description: `I co-funded a start-up doing VoIP business. The key tech is to find a way to traverse NAT so that it can establish a so called peer-to-peer VoIP communication path`,
+    description: `I co-funded a start-up doing VoIP business. The key tech is to find a way to traverse NAT so that it can establish a so called peer-to-peer VoIP communication path.`,
     imgSrc: '/static/images/sequence-port-forwarding.png',
     href: '/blog/voip',
   },
@@ -17,6 +17,12 @@ const projectsData: Project[] = [
     description: `This is my first personal web page using nextjs to show my blogs previously posted on WordPress.`,
     imgSrc: '/static/images/github-io-blog.png',
     href: '/blog/',
+  },
+  {
+    title: `Android Phone`,
+    description: `This was a long-lasting project I joined for 13 years.`,
+    imgSrc: '/static/images/black-phone.svg',
+    href: '/blog/android-phone',
   },
 ]
 

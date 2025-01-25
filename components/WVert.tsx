ac1a2b1cf8355774123cb6a31216c22b0eb993d5
hiltonchang <hiltonchiang@gmail.com>
2025-01-25 11:57:47 +0800

    vertical writing with red color

--- components/WVert.tsx
+++ components/WVert.tsx
@@ -7,7 +7,7 @@ interface Props {
 export default function WVert({ children }: Props) {
   return (
     <div className="pl-16">
-      <div className="prose max-w-none text-2xl [text-orientation:upright] [writing-mode:vertical-rl] dark:prose-invert xl:col-span-2">
+      <div className="prose max-w-none text-2xl text-red-400 [text-orientation:upright] [writing-mode:vertical-rl] dark:prose-invert xl:col-span-2">
         {children}
       </div>
     </div>

fc8010a68132ddc7b415aa041327885a4a2dc5b8
hiltonchang <hiltonchiang@gmail.com>
2025-01-26 15:45:39 +0800

    WVert pl-1

--- components/WVert.tsx
+++ components/WVert.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 export default function WVert({ children }: Props) {
   return (
-    <div className="pl-16">
-      <div className="prose max-w-none text-2xl text-red-400 [text-orientation:upright] [writing-mode:vertical-rl] dark:prose-invert xl:col-span-2">
+    <div className="align-middle pl-1">
+      <div className="prose align-middle max-w-none text-2xl text-red-400 [text-orientation:upright] [writing-mode:vertical-rl] dark:prose-invert xl:col-span-2">
         {children}
       </div>
     </div>

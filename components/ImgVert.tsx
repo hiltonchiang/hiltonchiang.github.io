416c93049dd9966af5339b4703be9a3ab19eed5c
hiltonchang <hiltonchiang@gmail.com>
2025-01-30 15:22:41 +0800

    tooltip, table for WVert

--- components/ImgVert.tsx
+++ components/ImgVert.tsx
@@ -16,13 +16,21 @@ const ImgVert = ({ texts, imgSrc, imgAlt, height = '60' }: Props) => {
       <div className="w-full transform-gpu overflow-hidden px-2 hover:scale-125 xl:my-1 xl:w-1/2 xl:px-2">
         <img alt={imgAlt} src={imgSrc} className={clsString} />
       </div>
-      <div className="pl-16">
-        <div className="prose mt-3 max-w-none text-2xl text-stone-500 [text-orientation:upright] [writing-mode:vertical-rl] dark:prose-invert dark:text-amber-300 xl:col-span-2">
-          {texts.map((t) => (
-            <p key={t}> {t} </p>
-          ))}
-        </div>
-      </div>
+      <table className="...table-fixed border-collapse items-center border border-stone-300 dark:border-slate-500">
+        <tbody>
+          <tr>
+            <td>
+              <div className="p-4 align-middle">
+                <div className="prose max-w-none align-middle text-2xl text-stone-500 [text-orientation:upright] [writing-mode:vertical-rl] dark:prose-invert dark:text-red-400 xl:col-span-2">
+                  {texts.map((t) => (
+                    <p key={t}> {t} </p>
+                  ))}
+                </div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </>
   )
 }
--- components/WVert.tsx
+++ components/WVert.tsx
@@ -6,11 +6,19 @@ interface Props {
 
 export default function WVert({ children }: Props) {
   return (
-    <div className="pl-1 align-middle">
-      <div className="prose max-w-none align-middle text-2xl text-stone-500 [text-orientation:upright] [writing-mode:vertical-rl] dark:prose-invert dark:text-red-400 xl:col-span-2">
-        {children}
-      </div>
-    </div>
+    <table className="...table-fixed border-collapse items-center border border-stone-300 dark:border-slate-500">
+      <tbody>
+        <tr>
+          <td>
+            <div className="p-4 align-middle">
+              <div className="prose max-w-none align-middle text-2xl text-stone-500 [text-orientation:upright] [writing-mode:vertical-rl] dark:prose-invert dark:text-red-400 xl:col-span-2">
+                {children}
+              </div>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   )
 }
 WVert.displayName = 'WVert'

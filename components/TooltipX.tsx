f94a6ac9abf1abd1f1e460a2f1de07b727696a77
hiltonchang <hiltonchiang@gmail.com>
2025-10-21 14:36:58 +0800

    modify family-root.mdx

--- components/TooltipX.tsx
+++ components/TooltipX.tsx
@@ -1,27 +1,27 @@
 'use client'
-import { ReactNode } from 'react'
-import { useTheme } from 'next-themes'
-import { useEffect, useState } from 'react'
+
+import { cn } from '@/lib/utils'
 
 interface Props {
-  contents: string
-  children: ReactNode
+  contents: string[]
 }
 
-export default function TooltipX({ contents, children }: Props) {
-  const { theme, setTheme, resolvedTheme } = useTheme()
-  const [mounted, setMounted] = useState(false)
-  useEffect(() => setMounted(true), [])
-  if (!mounted) {
-    return null
-  }
+export default function TooltipX({ contents }: Props) {
   return (
     <>
-      <div className="relative inline-block">
-        {children}
-        <div className="roup-hover:opacity-1 pointer-events-none absolute bottom-full left-1/2 mb-2 -translate-x-1/2 rounded bg-gray-800 p-2 text-sm text-white opacity-0 transition-opacity duration-300">
-          {contents}
-        </div>
+      <div
+        className={cn(
+          'bg-popover text-popover-foreground animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+          'pointer-events-none absolute bottom-full left-1/2 z-50 mb-2 -translate-x-6 translate-y-4 overflow-hidden',
+          'rounded rounded-md border bg-gray-800 p-2 px-3 py-1.5',
+          'text-base text-sm text-stone-300',
+          'opacity-0 shadow-md transition-opacity duration-300',
+          'group-hover:opacity-100 dark:text-lime-300'
+        )}
+      >
+        {contents.map((t) => (
+          <p key={t}> {t} </p>
+        ))}
       </div>
     </>
   )

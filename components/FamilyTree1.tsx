98b054ea70a49c231153d7267bf45dca5a072deb
hiltonchang <hiltonchiang@gmail.com>
2025-11-12 19:43:12 +0800

    omit family tree due to security

--- components/FamilyTree1.tsx
+++ components/FamilyTree1.tsx
@@ -30,66 +30,68 @@ title: 蔣氏家譜樹
         classDef TransparentSubgraph fill:#00000000,stroke-width:0
         classDef invisible fill:#0000, stroke:#0000, color:#0000
         %% GENERATION 7 %%
-        蔣以懋((蔣以懋)):::green e0@-->G4((以懋家<br/>入川第六代)):::blue
+        蔣以懋((蔣以懋)):::green -->G4((以懋家<br/>入川第六代)):::blue
+        L_蔣以懋_G4_0@{ animate: true }
         周氏((周氏)):::green ---G4
-        G4 e01@-->Fa1
+        G4 -->Fa1
+        L_G4_Fa1_0@{ animate: true }
         click G4 "以字輩"
-        Fa1((蔣超元)):::green e1@--> G3((超元家<br/>入川第七代)):::blue
+        Fa1((蔣超元)):::green --> G3((超元家<br/>入川第七代)):::blue
         click Fa1 "蔣超元:譜名仁孝<br/>生於1870年2月24日<br/>歿於民國32年（1943年）陰曆11月11日<br/>葬於雙江鎮老鸛嘴新房子山後"
+        L_Fa1_G3_0@{ animate: true }
         
         Mo1((蔣胡氏)):::green --- G3 
         click Mo1 "蔣胡氏<br/>生於1880年6月14日<br/>歿於民國46年（1957年)<br/>葬於雙江鎮老鸛嘴新房子山後"
     
         click G3 "仁字輩"
         %% GENERATION 8 %%
-        G3 e2@--長子--> Fa((蔣文彬)):::green e3@--> G2((文彬家<br/>入川第八代<br/>入臺第一代)):::blue
+        G3 --長子--> Fa((蔣文彬)):::green --> G2((文彬家<br/>入川第八代<br/>入臺第一代)):::blue
         click Fa "蔣文彬:譜名必兵<br/>生於民國11年（1922年）陰曆1月1日於四川潼南雙江鎮老鸛嘴新房子<br/>歿於民國73年（1984年）陽曆10月19日於臺北榮總<br/>葬於臺北觀音山<br/>配劉文筠,育有二子五女"
-
+        L_G3_Fa_0@{ animate: true }
+        L_Fa_G2_0@{ animate: true }
         Mo((劉文筠)):::green --- G2
         click Mo "劉文筠:譜名祖貞<br/>生於民國14年（1925年）陰曆6月15日於雙江鎮外北街10號<br/>歿於民國110年（2021年）陰曆1月14日於臺北家中<br/>與夫同葬於台北觀音山"
 
         click G2 "必字輩"
         %% GENERATION 9 %%
-        G2 e4@--> S0((蔣大毛)):::green
+        G2 --> S0((蔣大毛)):::green
+        L_G2_S0_0@{ animate: true }
         click S0 "蔣大毛<br/>生於民國35年（1946年）6月13日於重慶<br/>然未及7日即夭折"
-        G2 e5@--長女--> S1((蔣玉芳))
+        G2 --長女--> S1((蔣玉芳))
         click S1 "蔣玉芳<br/>生於民國37年（1948年）陰曆9月28日於上海<br/>適張亞明、︁育有一女張婷怡、︁一子張廼翔<br/>今居台北"
-        G2 e6@--長子--> Or((蔣玉華)):::green
+        L_G2_S1_0@{ animate: true }
+        G2 --長子--> Or((蔣玉華)):::green
         click Or "蔣玉華<br/>生於民國40年（1951年）陽曆6月1日於臺北<br/>卒於民國91年（2002年）10月1日於雲南<br/>葬於臺北五指山國軍公墓<br/>配楊鴻娟︁、︁育有一女蔣欣潔、︁一子蔣昌翰"
-        S0 e18@--> G1
-        S1 e19@--> G1
-        Or e20@--> G1((入臺第二代)):::blue
+        L_G2_Or_0@{ animate: true }
+        S0 --> G1
+        L_S0_G1_0@{ animate: true }
+        S1 --> G1
+        L_S1_G1_0@{ animate: true }
+        Or --> G1((入臺第二代)):::blue
+        L_Or_G1_0@{ animate: true }
         click G1 "愈字輩"
-        G2 e7@--次女--> S3((蔣玉萍))
-        G2 e8@--三女--> S4((蔣玉遐))
-        G2 e9@--四女--> S5((蔣玉蘭))
-        G2 e10@--次子--> S6((蔣玉明))
-        G2 e11@--五女--> S7((蔣德純))
-        S3 e21@--> G1
-        S4 e22@--> G1
-        S5 e23@--> G1
-        S6 e24@--> G1
-        S7 e25@--> G1
-        e0@{ animate: true } 
-        e01@{ animate: true } 
-        e1@{ animate: true } 
-        e2@{ animate: true } 
-        e3@{ animate: true } 
-        e4@{ animate: true } 
-        e5@{ animate: true } 
-        e6@{ animate: true } 
-        e7@{ animate: true } 
-        e8@{ animate: true } 
-        e9@{ animate: true } 
-        e10@{ animate: true } 
-        e11@{ animate: true } 
-        e19@{ animate: true } 
-        e20@{ animate: true } 
-        e21@{ animate: true } 
-        e22@{ animate: true } 
-        e23@{ animate: true } 
-        e24@{ animate: true } 
-        e25@{ animate: true } 
+        G2 --次女--> S3((蔣玉萍))
+        L_G2_S3_0@{ animate: true }
+        G2 --三女--> S4((蔣玉遐))
+        L_G2_S4_0@{ animate: true }
+        G2 --四女--> S5((蔣玉蘭))
+        L_G2_S5_0@{ animate: true }
+        G2 --次子--> S6((蔣玉明))
+        L_G2_S6_0@{ animate: true }
+        G2 --五女--> S7((蔣德純))
+        L_G2_S7_0@{ animate: true }
+        S3 --> G1
+        S4 --> G1
+        S5 --> G1
+        S6 --> G1
+        S7 --> G1
+         
+        L_S3_G1_0@{ animate: true }
+        L_S4_G1_0@{ animate: true }
+        L_S5_G1_0@{ animate: true }
+        L_S6_G1_0@{ animate: true }
+        L_S7_G1_0@{ animate: true }
+         
 `
   return <Mermaid chart={familyTreeCode} mDevice={false} />
 }
--- components/MermaidFamily.tsx
+++ components/MermaidFamily.tsx
@@ -3,7 +3,116 @@ import { useState, useRef, useEffect } from 'react'
 import mermaid from 'mermaid'
 import { useSearchParams } from 'next/navigation'
 import { useTheme } from 'next-themes'
+import { gsap } from 'gsap'
+import { MotionPathPlugin } from 'gsap/MotionPathPlugin'
+import parse from 'parse-svg-path'
 import * as d3 from 'd3'
+/*
+ * Transform return types
+ */
+interface TypeMatrix {
+  type: string
+  a: number
+  b: number
+  c: number
+  d: number
+  e: number
+  f: number
+}
+interface TypeTranslateScale {
+  type: string
+  x: number
+  y: number
+}
+interface TypeRotate {
+  type: string
+  angle: number
+  x: number
+  y: number
+}
+interface TypeSkew {
+  type: string
+  angle: number
+}
+type TypeTransform = TypeMatrix | TypeTranslateScale | TypeRotate | TypeSkew
+
+function parseSvgTransform(transformString) {
+  const svgns = 'http://www.w3.org/2000/svg'
+  // Create a temporary SVG element to apply the transform to
+  const tempSvg = document.createElementNS(svgns, 'svg')
+  const tempRect = document.createElementNS(svgns, 'rect')
+  // Get the transform attribute value from the target element
+  // const transformString = element.getAttribute('transform')
+
+  if (!transformString) {
+    return [] // No transform attribute found
+  }
+
+  // Set the transform attribute on the temporary element
+  tempRect.setAttribute('transform', transformString)
+  // Access the SVGTransformList through the DOM API
+  const transformList = tempRect.transform.baseVal
+  const parsedTransforms: TypeTransform[] = []
+
+  for (let i = 0; i < transformList.numberOfItems; i++) {
+    const transform = transformList.getItem(i)
+    const matrix = transform.matrix
+
+    switch (transform.type) {
+      case SVGTransform.SVG_TRANSFORM_MATRIX:
+        parsedTransforms.push({
+          type: 'matrix',
+          a: matrix.a,
+          b: matrix.b,
+          c: matrix.c,
+          d: matrix.d,
+          e: matrix.e,
+          f: matrix.f,
+        })
+        break
+      case SVGTransform.SVG_TRANSFORM_TRANSLATE:
+        parsedTransforms.push({
+          type: 'translate',
+          x: matrix.e,
+          y: matrix.f,
+        })
+        break
+      case SVGTransform.SVG_TRANSFORM_SCALE:
+        parsedTransforms.push({
+          type: 'scale',
+          x: matrix.a,
+          y: matrix.d,
+        })
+        break
+      case SVGTransform.SVG_TRANSFORM_ROTATE:
+        // Note: The matrix for rotate will be more complex,
+        // but the SVGTransform object directly provides the angle.
+        parsedTransforms.push({
+          type: 'rotate',
+          angle: transform.angle,
+          x: transform.matrix.e, // Rotation center x (if specified)
+          y: transform.matrix.f, // Rotation center y (if specified)
+        })
+        break
+      case SVGTransform.SVG_TRANSFORM_SKEWX:
+        parsedTransforms.push({
+          type: 'skewX',
+          angle: transform.angle,
+        })
+        break
+      case SVGTransform.SVG_TRANSFORM_SKEWY:
+        parsedTransforms.push({
+          type: 'skewY',
+          angle: transform.angle,
+        })
+        break
+      default:
+        // Handle other transform types or unknown types if necessary
+        break
+    }
+  }
+  return parsedTransforms
+}
 
 /*
  * using d3 to zoom entire svg
@@ -48,7 +157,21 @@ function getSVG(
   }
   return [svgWidth, svgHeight, svgRoot]
 }
-
+function getG(
+  gId: string
+): [string, d3.Selection<SVGSVGElement, unknown, HTMLElement, unknown> | null] {
+  const gs = d3.selectAll('g')
+  const nodes = gs.nodes()
+  let g
+  for (let i = 0; i < nodes.length; i++) {
+    const id = d3.select(nodes[i]).attr('id')
+    if (id !== null && id === gId) {
+      g = d3.select(nodes[i])
+      break
+    }
+  }
+  return [gId, g]
+}
 function d3HandleZoom(id) {
   const [svgWidth, svgHeight, svgRoot] = getSVG(id)
   if (svgRoot !== null) {
@@ -77,8 +200,22 @@ function d3HandleZoom(id) {
 /*
  * using d3 to find <span class='edgeLabel'
  */
-function d3HandleEdgeLabel(id) {
+interface CellObj {
+  group: string
+  index: number
+}
+interface EdgeLabelObj {
+  g: d3.Selection<SVGGElement, unknown, HTMLElement | null, unknown>
+  transform: TypeTransform[]
+  dataId: string
+  from: string
+  to: string
+  parent: CellObj[]
+  type: string
+}
+function d3HandleEdgeLabel(id, map) {
   const group = d3.selectAll('g')
+  const M: EdgeLabelObj[] = []
   group.each(function (d, i, nodes) {
     /*
      * 'this' refers to the current circle element
@@ -90,10 +227,33 @@ function d3HandleEdgeLabel(id) {
     if (edge === 'edgeLabel') {
       const tr = d3.select(this).attr('transform')
       if (tr != null) {
+        const g: d3.Selection<SVGGElement, unknown, HTMLElement | null, unknown> = d3.select<
+          SVGGElement,
+          unknown
+        >(this as SVGGElement)
+        const gg: d3.Selection<SVGGElement, unknown, HTMLElement | null, unknown> =
+          g.select<SVGGElement>('g')
+        const dataId: string = gg?.attr('data-id')
+        const transform = parseSvgTransform(d3.select(this).attr('transform'))
         const span = d3.select(this).select('span').attr('class')
         if (span !== null && span === 'edgeLabel') {
           const p = d3.select(this).select('span').select('p')
           if (!p.empty()) {
+            if (dataId) {
+              const D = dataId?.split('_')
+              if (D.length >= 3) {
+                const O: EdgeLabelObj = {
+                  g: gg,
+                  transform: transform,
+                  dataId: dataId,
+                  from: D[1],
+                  to: D[2],
+                  parent: [],
+                  type: 'EdgeLabelObj',
+                }
+                M.push(O)
+              }
+            }
             d3.select(this)
               .on('mouseover', function (event, d) {
                 const tr = d3.select(this).attr('transform')
@@ -108,6 +268,7 @@ function d3HandleEdgeLabel(id) {
       }
     }
   })
+  map.set('edgeLabels', M)
 }
 /*
  * using d3 to find anchor and display Tooltips
@@ -174,6 +335,9 @@ function d3HandleAnchor(id, md) {
       })
   }
 }
+/*
+ * Display Title
+ */
 function showTitle(id, md, theme) {
   const [w, h, svg] = getSVG(id)
   if (svg === null) return
@@ -182,8 +346,12 @@ function showTitle(id, md, theme) {
   text?.attr('font-size', '24px')
   const color = theme === 'dark' ? 'white' : 'black'
   text?.attr('fill', '#777')
+  text?.attr('id', 'title-' + id)
 }
-function d3AppendResetSvg(id) {
+/*
+ * add a reset button to reset rezied svg to its origin
+ */
+function d3AppendResetSvg(id, map) {
   const [w, h, svg] = getSVG(id)
   if (svg !== null) {
     const resetSVG = svg
@@ -224,9 +392,14 @@ function d3AppendResetSvg(id) {
             })
         }
       })
+      .on('dblclick', function (e, d) {
+        e.preventDefault()
+        e.stopPropagation()
+        AnimationOne(id, map, null)
+      })
       .on('mouseover', function (e, d) {
         d3.select(this).style('cursor', 'pointer')
-        tooltip.html('Reset<br/>Graph').style('opacity', 1)
+        tooltip.html('Click<br/>OR<br/>Double Click').style('opacity', 1)
       })
       .on('mousemove', function (e, d) {
         tooltip.style('left', e.pageX + 10 + 'px').style('top', e.pageY - 10 + 'px')
@@ -236,9 +409,283 @@ function d3AppendResetSvg(id) {
       })
   }
 }
+
+/*
+ *
+ */
+interface PathObj {
+  path: d3.Selection<SVGPathElement, unknown, HTMLElement | null, unknown>
+  pathCmd: Array<Array<string | number>>
+  id: string
+  from: string
+  to: string
+  child: CellObj[]
+  parent: CellObj[]
+  type: string
+}
+interface NodeObj {
+  node: d3.Selection<SVGGElement, unknown, HTMLElement | null, unknown>
+  transform: TypeTransform[]
+  id: string
+  name: string
+  child: CellObj[]
+  parent: CellObj[]
+  type: string
+}
+function findRelations(svgId, map) {
+  const [w, h, svg] = getSVG(svgId)
+  if (svg === null) return
+  const Ppaths: PathObj[] = []
+  const Nnodes: NodeObj[] = []
+  const group = svg.selectAll('g')
+  group.each(function (d, i, nodes) {
+    const cls = d3.select(this).attr('class')
+    if (cls === 'edgePaths') {
+      const paths = d3.select(this).selectAll('path')
+      paths.each(function (d, i, nodes) {
+        const path: d3.Selection<SVGPathElement, unknown, HTMLElement | null, unknown> = d3.select<
+          SVGPathElement,
+          unknown
+        >(this as SVGPathElement)
+        const id = d3.select(this).attr('id')
+        if (id != null) {
+          const s = id.split('_')
+          if (s.length >= 3) {
+            const d = d3.select(this).attr('d')
+            const pathCommand = parse(d)
+            const O: PathObj = {
+              path: path,
+              pathCmd: pathCommand,
+              id: id,
+              from: s[1],
+              to: s[2],
+              child: [],
+              parent: [],
+              type: 'PathObj',
+            }
+            Ppaths.push(O)
+          }
+        }
+      })
+    } else if (cls === 'nodes') {
+      const gs = d3.select(this).selectAll('g')
+      gs.each(function (d, i, nodes) {
+        const id = d3.select(this).attr('id')
+        if (id !== null) {
+          const s = id.split('-')
+          const n: d3.Selection<SVGGElement, unknown, HTMLElement | null, unknown> = d3.select<
+            SVGGElement,
+            unknown
+          >(this as SVGGElement)
+          const transform = parseSvgTransform(d3.select(this).attr('transform'))
+          const O: NodeObj = {
+            node: n,
+            transform: transform,
+            id: id,
+            name: s[1],
+            child: [],
+            parent: [],
+            type: 'NodeObj',
+          }
+          Nnodes.push(O)
+        }
+      })
+    }
+  })
+  map.set('Nodes', Nnodes)
+  map.set('edgePaths', Ppaths)
+  /*
+   * sort map
+   */
+  const nodes: NodeObj[] = map.get('Nodes')
+  const paths: PathObj[] = map.get('edgePaths')
+  const labels: EdgeLabelObj[] = map.get('edgeLabels')
+  for (let i = 0; i < nodes.length; i++) {
+    const nodeName = nodes[i].name
+    for (let j = 0; j < paths.length; j++) {
+      const Op: CellObj = {
+        group: 'edgePaths',
+        index: j,
+      }
+      const On: CellObj = {
+        group: 'Nodes',
+        index: i,
+      }
+      if (nodeName === paths[j].from) {
+        nodes[i].child.push(Op)
+        paths[j].parent.push(On)
+      }
+      if (nodeName === paths[j].to) {
+        nodes[i].parent.push(Op)
+        paths[j].child.push(On)
+      }
+    }
+  }
+  for (let i = 0; i < labels.length; i++) {
+    const id = labels[i].dataId
+    for (let j = 0; j < paths.length; j++) {
+      const Op: CellObj = {
+        group: 'edgePaths',
+        index: j,
+      }
+      const Ol: CellObj = {
+        group: 'edgeLabels',
+        index: i,
+      }
+      if (id === paths[j].id) {
+        paths[j].child.push(Ol)
+        labels[i].parent.push(Op)
+        break
+      }
+    }
+  }
+}
+/*
+ * use gsap to animate
+ */
+function gsapAnimate(id, map) {
+  const [w, h, svg] = getSVG(id)
+  if (svg === null) return
+  gsap.set('#' + id, { opacity: 0 })
+  gsap.to('#' + id, {
+    duration: 3, // Animation duration in seconds
+    opacity: 1,
+    ease: 'sine.inOut',
+  })
+
+  gsap.to('#title-' + id, {
+    x: -25,
+    duration: 3, // Animation duration in seconds
+    yoyo: true,
+    repeat: -1,
+    ease: 'sine.inOut', // Easing function
+  })
+}
+/*
+ * Animation One
+ *
+ */
+function findRoot(id, map): Array<PathObj | NodeObj> {
+  const [w, h, svg] = getSVG(id)
+  const M: Array<PathObj | NodeObj> = []
+  if (svg === null) {
+    return M
+  }
+  if (id !== map.get('diagramId')) {
+    return M
+  }
+  const N: NodeObj[] = map.get('Nodes')
+  const P: PathObj[] = map.get('edgePaths')
+  for (let i = 0; i < N.length; i++) {
+    if (N[i].parent.length === 0) {
+      M.push(N[i])
+    }
+  }
+  for (let i = 0; i < P.length; i++) {
+    if (P[i].parent.length === 0) {
+      M.push(P[i])
+    }
+  }
+  console.log('root obj ', M)
+  return M
+}
+function AnimationOne(id, map, curRoots: Array<PathObj | NodeObj> | null) {
+  const [w, h, svg] = getSVG(id)
+  if (svg === null) return
+  if (id !== map.get('diagramId')) return
+  const N: NodeObj[] = map.get('Nodes')
+  const P: PathObj[] = map.get('edgePaths')
+  const L: EdgeLabelObj[] = map.get('edgelabels')
+  if (N.length == 0) return
+  if (P.length == 0) return
+  if (curRoots === null) {
+    curRoots = findRoot(id, map)
+  }
+  if (curRoots.length === 0) return
+  let totalAnimations = 0
+  let finished = 0
+
+  function allComplete() {
+    finished++
+    if (finished === totalAnimations) {
+      console.log('All animations are complete!')
+      const M: Array<PathObj | NodeObj> = []
+      if (curRoots != null) {
+        for (let i = 0; i < curRoots.length; i++) {
+          for (let j = 0; j < curRoots[i]?.child?.length; j++) {
+            const cCell = curRoots[i].child[j]
+            if (cCell.group === 'Nodes') {
+              const n = N[cCell.index].child
+              for (let k = 0; k < n.length; k++) {
+                if (n[k].group === 'edgePaths') {
+                  M.push(P[n[k].index])
+                }
+              }
+            } else if (cCell.group === 'edgePaths') {
+              const p = P[cCell.index].child
+              for (let k = 0; k < p.length; k++) {
+                if (p[k].group === 'Nodes') {
+                  M.push(N[p[k].index])
+                }
+              }
+            }
+          }
+        }
+      }
+      AnimationOne(id, map, M)
+    }
+  }
+  for (let i = 0; i < curRoots.length; i++) {
+    if (curRoots[i].type === 'NodeObj') {
+      const Root = curRoots[i] as NodeObj
+      totalAnimations = Root.child.length
+      finished = 0
+      for (let j = 0; j < Root.child.length; j++) {
+        const cIdx = Root.child[j].index
+        const circle = Root.node.append('circle')
+        const path = P[cIdx].path.node()
+        circle
+          .attr('cx', 0) // X-coordinate of the center
+          .attr('cy', 0) // Y-coordinate of the center
+          .attr('r', 5) // Radius of the circle
+          .attr('style', 'fill:#ff0000 !important') // Fill color
+          .attr('stroke', 'black') // Border color
+        if (path !== null) {
+          gsap.to(circle.node(), {
+            duration: 1,
+            ease: 'power1.inOut',
+            overwrite: 'auto',
+            immediateRender: false,
+            motionPath: {
+              path: path,
+              align: path,
+              autoRotate: false,
+              alignOrigin: [0.5, 0.5],
+            },
+            onStart: () => {},
+            onComplete: () => {
+              circle.remove()
+              allComplete()
+            },
+            onUpdate: () => {
+              // console.log('animating...')
+            },
+          })
+        }
+      }
+    } else if (curRoots[i].type === 'PathObj') {
+      const Root = curRoots[i] as PathObj
+      console.log('Path without parent ', Root)
+    }
+  }
+}
+/*
+ * main program to return a svg
+ */
 const Mermaid = ({ chart, mDevice }) => {
   const mermaidRef = useRef<HTMLDivElement | null>(null)
   const { theme, setTheme, resolvedTheme } = useTheme()
+  const [map, setMap] = useState(new Map())
   const searchParams = useSearchParams()
   const msg = searchParams.get('msg')
   useEffect(() => {
@@ -249,17 +696,22 @@ const Mermaid = ({ chart, mDevice }) => {
         try {
           const { svg, bindFunctions } = await mermaid.render(diagramId, chart)
           if (mermaidRef.current !== null) {
+            map.set('diagramId', diagramId)
             mermaidRef.current.innerHTML = svg
             bindFunctions?.(mermaidRef.current)
             if (mDevice === false) {
               d3HandleZoom(diagramId)
             }
             d3HandleAnchor(diagramId, mDevice)
-            d3HandleEdgeLabel(diagramId)
+            d3HandleEdgeLabel(diagramId, map)
             if (mDevice === false) {
-              d3AppendResetSvg(diagramId)
+              d3AppendResetSvg(diagramId, map)
             }
             showTitle(diagramId, mDevice, resolvedTheme)
+            findRelations(diagramId, map)
+            gsapAnimate(diagramId, map)
+            gsap.registerPlugin(MotionPathPlugin)
+            console.log(diagramId, 'map ', map)
           }
         } catch (error) {
           console.error('Mermaid render error:', error)
@@ -270,7 +722,7 @@ const Mermaid = ({ chart, mDevice }) => {
       }
       renderChart()
     }
-  }, [chart, msg, mDevice]) // Re-render if the chart code changes
+  }, [chart, msg, mDevice, resolvedTheme, map]) // Re-render if the chart code changes
 
   return <div ref={mermaidRef}></div>
 }

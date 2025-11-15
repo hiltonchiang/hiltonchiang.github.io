25b09a5c286c34e38aa1606ed707008759a00b2f
hiltonchang <hiltonchiang@gmail.com>
2025-11-15 16:23:52 +0800

    modified a little

--- components/DialogFamily.tsx
+++ components/DialogFamily.tsx
@@ -469,7 +469,6 @@ const DialogFamily = () => {
                           required
                           autoComplete="current-password"
                           className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
-                          onInput={(e) => {console.log(e.target.value)}}
                         />
                       </div>
                     </div>

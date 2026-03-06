af114ea1b9b28b1a677e612240f0142c204d9bf2
hiltonchang <hiltonchiang@gmail.com>
2026-03-07 07:37:01 +0800

    corrected eslint format check error

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function PageTitle({ children }: Props) {
   return (
-    <h1 className="text-3xl font-extrabold font-ming leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+    <h1 className="font-ming text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
       <HOVER>{children}</HOVER>
     </h1>
   )
